c04a0940ef8cd522f422cd3a2058218fa2e293c6
sunshinezzz <605855436@qq.com>
2025-04-06 19:46:03 +0800

    user添加

--- app/router.lua
+++ app/router.lua
@@ -1,10 +1,9 @@
 -- Comment: 路由管理相关
 
 local loginRouter = require("app.routes.login")
--- local userRouter = require("app.routes.user")
+local userRouter = require("app.routes.user")
 
 return function(app)
-	-- simple router: render html, visit "/" or "/?name=foo&desc=bar
 	app:get("/", function(req, res, next)
 		local data = {
 			name =  req.query.name or "lor",
@@ -14,7 +13,6 @@ return function(app)
 	end)
 
 	app:use("/login", loginRouter())
-	-- group router: 对以`/user`开始的请求做过滤处理
-	-- app:use("/user", userRouter())
+	app:use("/user", userRouter())
 end
 
